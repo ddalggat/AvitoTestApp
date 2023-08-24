9703cd25556ee815946b0de488a2a1a195248d46
Dalgat Abdullabekov <abd.dalgat@gmail.com>
2023-08-24 18:57:52 +0300

    third

---
+++ AvitoTestApp/Models/MainPage.swift
@@ -0,0 +1,19 @@
+//
+//  MainPage.swift
+//  AvitoTestApp
+//
+//  Created by Abdullabekov Dalgat on 8/24/23.
+//
+
+import Foundation
+
+struct MainPage: Codable {
+    let catalog: [MainInfo]
+}
+
+struct MainInfo: Codable {
+    let id: String
+    let title: String
+    let price: String
+    let location: String
+}
