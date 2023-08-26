263e5a753ad34e68fd21d17da7781c308a6d8e0b
Dalgat Abdullabekov <abd.dalgat@gmail.com>
2023-08-26 20:50:35 +0300

    add View

---
+++ AvitoTestApp/Views/Catalog.swift
@@ -0,0 +1,21 @@
+//
+//  Catalog.swift
+//  AvitoTestApp
+//
+//  Created by Abdullabekov Dalgat on 8/26/23.
+//
+
+import UIKit
+
+extension UIView {
+    func addSubviews(_ views: UIView...) {
+        views.forEach({
+            addSubview($0)
+        })
+    }
+}
+
+
+class Catalog: UIView {
+    
+}
