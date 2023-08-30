11e833c15c6e0386a633fc8bee9b80754bef91b3
Dalgat Abdullabekov <abd.dalgat@gmail.com>
2023-08-30 23:25:03 +0300

    Initial Commit

---
+++ AvitoTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoTestApp
+//
+//  Created by Abdullabekov Dalgat on 8/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
